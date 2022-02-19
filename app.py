c6b7f2727aa561ad4a119fc0b646be40475793e8
Vaibhav Choudhary <vaibhavch29@gmail.com>
2022-02-19 13:33:50 +0530

    updated all the subjects of 1-6 sem

--- app.py
+++ app.py
@@ -30,11 +30,38 @@ def calc(sem):
     labs = {}
     GPA = 0
     flag = 0
+    credits = 0
     col1, col2 = st.columns(2)
 
     if sem == 1 :
         subjects = { 'App. Maths-I' : 4, 'App. Physics-I' : 3, 'Manufacturing Processes' : 3, 'Electrical Tech.' : 3, 'Human Values' : 1, 'Fundamentals of Computing' : 2, 'App. Chemistry' : 3 }
         labs = { 'App. Physics Lab-I' : 1, 'Elecrical Tech. Lab' : 1, 'Workshop' : 2, 'Engg. Graphics Lab' : 2, 'FOC Lab' : 1, 'App. Chemistry Lab' : 1 }
+        credits = 27
+
+    elif sem == 2:
+        subjects = { 'App. Maths-II' : 4, 'App. Physics-II' : 3, 'Electronic Devices' : 3, 'Intro To Programming' : 3, 'Engineering Mechanics' : 3, 'Communication Skills' : 3, 'Environmental Studies' : 3 }
+        labs = { 'App. Physics Lab-II' : 1, 'Programming Lab' : 1, 'Electronics Lab' : 1, 'Engineering Mechanics Lab' : 1, 'EVS Lab' : 1 }
+        credits = 27
+
+    elif sem == 3:
+        subjects = { 'App. Maths-III' : 4, 'Foundation of CS' : 4, 'Switching Theory & Logic Design' : 4, 'Circuits & Systems' : 4, 'Data Structures' : 4, 'Computer Graphics & Multimedia' : 4, }
+        labs = { 'STLD Lab' : 1, 'Data Stucture Lab' : 1, 'Circuits & Systems Lab' : 1, 'CGMM Lab' : 1 }
+        credits = 28
+
+    elif sem == 4:
+        subjects = { 'App. Maths-IV' : 4, 'Computer Organisation & Architecture' : 4, 'Theory of Computation' : 4, 'Database Management' : 4, 'Object Oriented Programming' : 3, 'Control Systems' : 4 }
+        labs = { 'App. Maths Lab' : 1, 'COA Lab' : 1, 'DBMS Lab' : 1, 'OOPS Lab' : 1, 'Control Systems Lab' : 1 }
+        credits = 28
+
+    elif sem == 5:
+        subjects = { 'Algo. Design & Analysis' : 4, 'Software Engineering' : 4, 'Java Programming' : 4, 'Industrial Management' : 3, 'Communication Systems' : 4, 'Communication Skills' : 1 }
+        labs = { 'Algo. Design Lab' : 1, 'Software Engineering Lab' : 1, 'Java Programming Lab' : 1, 'In-house Workshop' : 1, 'Communication Systems Lab' : 1, 'Communication Skills Lab' : 1 }
+        credits = 26
+
+    elif sem == 6:
+        subjects = { 'Compiler Design' : 4, 'Operating Systems' : 4, 'Data Communication & Networks' : 4, 'Web Engineering' : 3, 'Artificial Intelligence' : 4, 'Microprocessors & Microcontrollers' : 4, }
+        labs = { 'Operating Systems Lab' : 1, 'Networks Lab' : 1, 'Web Engineering Lab' : 1, 'Microprocessor & Microcontroller Lab' : 1 }
+        credits = 27
 
     with col1:
         with st.expander("Theory Subjects"):
@@ -57,7 +84,7 @@ def calc(sem):
     if flag:
         st.warning("You haven't entered the marks of all subjects!")
 
-    GPA = GPA/27
+    GPA = GPA / credits
     return GPA
 
     
@@ -93,6 +120,8 @@ with st.container():
                 msg = "Your GPA: {}".format(str(round(GPA,2)))
                 st.markdown(f"<h3 style='text-align: center; '>{msg}</h3>", unsafe_allow_html=True)
                 if GPA >= 8.0 :
+                    st.balloons()
+                    st.balloons()
                     st.balloons()
 
 
